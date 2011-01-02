d370d3b3c48e39d7d5cc59490a983df7929207ab
Pacha Camac <pachacamac@lavabit.com>
2011-01-02 23:26:48 +0100

    added a comment about three different devices in kinect. need to refactor device.rb

--- lib/rinect/device.rb
+++ lib/rinect/device.rb
@@ -4,6 +4,14 @@ module Rinect
     attr_reader   :connect, :get
     
     def initialize
+      # TODO: we need to refactor this because the kinect cam contains actually three devices.
+      #       lsusb | grep Microsoft: 
+      #         Bus 001 Device 007: ID 045e:02ae Microsoft Corp. 
+      #         Bus 001 Device 006: ID 045e:02ad Microsoft Corp. 
+      #         Bus 001 Device 005: ID 045e:02b0 Microsoft Corp. 
+      #       Only the Motor-/LED-/Accel-device has the product id 0x02b0
+      #       I guess the other two are RGB- and depth cam.
+    
       @device ||= USB.devices.select{|d| d.idVendor==0x045e && d.idProduct==0x02b0}.first
       @handle = nil
       @state = :off
