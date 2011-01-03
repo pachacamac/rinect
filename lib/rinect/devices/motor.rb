ca074dc4a35e5b9905696fd9b5a2b17eb6286ccf
meltingice <meltingice8917@gmail.com>
2011-01-02 22:47:52 -0500

    Removed accelerometer code from Motor class since it has a dedicated class now

--- lib/rinect/devices/motor.rb
+++ lib/rinect/devices/motor.rb
@@ -15,19 +15,6 @@ module Rinect
       @state
     end
     
-    def read_accelerometer
-      data = " " * 10
-      @handle.usb_control_msg(0xC0, 0x32, 0x0, 0x0, data, 10)
-
-      result = {
-        :x => (data[2].unpack('C') << 8) | data[3].unpack('C'),
-        :y => (data[4].unpack('C') << 8) | data[5].unpack('C'),
-        :z => (data[6].unpack('C') << 8) | data[7].unpack('C'),
-        :tilt_angle => data[8].unpack('c'),
-        :tilt_status => data[9].unpack('c')
-      }
-    end
-    
     def to_s
       "Current pitch: #{@state.to_s}"
     end
